532a85a6ff4c998e9755faf290455dda6be04fca
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-06-07 13:50:11 -0500

    express server added

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express'),
+      app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3300, function () {
+  console.log('Listening on port 3300!')
+})
