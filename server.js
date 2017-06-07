0fea7f03e8a63e68d1a549188e6efde5c3e0ce2f
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-06-07 14:46:45 -0500

    server update

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-var express = require('express'),
-    app = express();
-
-var router = express.Router();
+const express = require('express'),
+      app = express(),
+      router = express.Router(),
+      compression = require('compression');
 
 router.get('/', function(req, res) {
-  res.end('Lorem:)');
+  res.end('Lorem');
 });
 
+app.use(express.static(__dirname + '/public'));
+app.use(compression());
 app.use(router);
 
 app.listen(3300);
