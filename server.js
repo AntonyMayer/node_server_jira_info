173ca9fd9e196b1a85bedaac1f3a020528685711
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-06-08 10:03:28 -0500

    rebuild

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express'),
       app = express(),
-      router = express.Router(),
-      compression = require('compression');
+      router = express.Router();
 
 router.get('/', function(req, res) {
-  res.end('Lorem');
+    console.log('Connected');
 });
 
 app.use(express.static(__dirname + '/public'));
-app.use(compression());
 app.use(router);
 
 app.listen(3300);
