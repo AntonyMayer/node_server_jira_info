3357a852aa16560520da6c49f7d11a32b707da2a
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-06-08 11:32:59 -0500

    server post functionality

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express'),
+      bodyParser= require('body-parser'),
       app = express(),
       router = express.Router();
 
-router.get('/', function(req, res) {
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'));
+app.use(router);
+
+router.get('/', (req, res) => {
     console.log('Connected');
 });
 
-app.use(express.static(__dirname + '/public'));
-app.use(router);
+router.post('/requests', (req, res) => {
+  console.log('user request send');
+  console.log(req.body);
+  res.end('Lorem');
+})
 
 app.listen(3300);
