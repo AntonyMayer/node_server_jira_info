6f865d45bd199de797c549666be4ca2581504174
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-06-08 12:57:02 -0500

    server upd

--- server.js
+++ server.js
@@ -4,23 +4,35 @@ const express = require('express'),
       app = express(),
       router = express.Router();
 
+/**
+ * Extending defaults
+ */
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'));
 app.use(router);
 
+/**
+ * Handle get requests
+ */
 router.get('/', (req, res) => {
-    console.log('Connected');
+    // do smth:)
 });
 
+/**
+ * Handle post requests
+ */
 router.post('/requests', (req, res) => {
-  console.log('user request send');
+  console.log('post request received');
   console.log(req.body);
-  res.end('Lorem');
+  res.end('Done!');
 });
 
+/**
+ * Connecting to DB and starting server if connection was succesfull 
+ */
 MongoClient.connect("mongodb://localhost:27017/jiraStat", function(err, db) {
     if (err) throw err;
-    console.log('Connected to DB');
+    console.log('>> Connected to DB');
+    app.listen(3300);
+    console.log('>> Server started');
 });
-
-app.listen(3300);
